8c88e63cf3e2c96145dd28a8205574406952bcb7
ashish200729 <darkaneesport@gmail.com>
2025-12-02 15:14:47 +0530

    Theme and styling

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,13 +1,12 @@
 import { RegisterForm } from "@/features/auth/components/register-form";
 import { requireUnAuth } from "@/lib/auth-utils";
 
+
 const SignupPage = async () => {
     await requireUnAuth();
 
     return (
-        <div>
-            <RegisterForm />
-        </div>
+        <RegisterForm />
     )
 }
 
